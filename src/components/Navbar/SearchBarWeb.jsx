917608141cbc2b256885edf5aedf562b1b2b256b
Isakov Khojiakbarkhon <73178602+Davronbek25@users.noreply.github.com>
2024-11-30 15:55:00 +0100

    seach bar web designed

--- src/components/Navbar/SearchBarWeb.jsx
+++ src/components/Navbar/SearchBarWeb.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const SearchBarWeb = () => {
   return (
-    <div>
+    <div id='searchBarWeb'>
         <input type="text" placeholder='Search for a song' />
+        <i class="bi bi-search"></i>
     </div>
   )
 }
