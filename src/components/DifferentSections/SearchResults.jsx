2eaa371bc13f5fd931df0dc753eb9b2f9d9bf484
Isakov Khojiakbarkhon <73178602+Davronbek25@users.noreply.github.com>
2025-01-27 16:52:50 +0100

    implemented search results as music cards

--- src/components/DifferentSections/SearchResults.jsx
+++ src/components/DifferentSections/SearchResults.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
+import AlbumCard from '../Main/AlbumCard'
 
 const SearchResults = () => {
   const location = useLocation()
   const { results } = location.state || {results: [] }
+  console.log(results)
 
   return (
-    <div id='searchResults'>
-      {results.length > 0 ? (
-        results.map((result, index) => (
-          <div key={index}>
-            <p>{result.title}</p>
-          </div>
-        ))
-      ): (
-        <p>No results found</p>
-      )}
+    <div id='searchResults' className='container list-cards pt-3'>
+        {results.length > 0 ? (
+          results.map((result, index) => (
+            <div key={index} className="row">
+              <AlbumCard songCard={result} />
+            </div>
+          ))
+        ): (
+          <h3 className='text-white text-align-center'>No songs found!</h3>
+        )}
     </div>
   )
 }
--- src/components/Main/AlbumCard.jsx
+++ src/components/Main/AlbumCard.jsx
@@ -2,19 +2,22 @@ import React, { useContext } from "react";
 import { SongsContext } from "../../context/SongsContextProvider";
 import uniqid from "uniqid";
 
-const AlbumCard = ({ song, start }) => {
+const AlbumCard = ({ song, start, songCard }) => {
   let songsContext = useContext(SongsContext);
   let songIdHandler = songsContext[1];
   let songId = songsContext[2];
   let isPlaying = songsContext[3];
   let idSong = uniqid();
+  let cardSong = songCard? songCard : song[start]
+  let colSpecs = songCard? 'col' : "col col-lg-4 col-md-6 col-sm-12"
+  console.log(cardSong, 'cardsong')
   return (
-    <div className="col col-lg-4 col-md-6 col-sm-12">
-      <div className="card mb-3" style={{ maxWidth: "540px" }}>
+    <div className={colSpecs}>
+      <div className="card mb-3 m-auto" style={{ maxWidth: cardSong? "740px" : "540px" }}>
         <div className="row flex-nowrap g-0">
           <div className="col-md-4">
             <img
-              src={song[start].album.cover_big}
+              src={cardSong.album.cover_big}
               className="img-fluid rounded-start"
               alt="..."
             />
@@ -22,19 +25,19 @@ const AlbumCard = ({ song, start }) => {
           <div className="col d-flex position-relative">
           <div className="card-text ps-3 w-75">
               <p className="ps-3 text-white m-0">
-                {song[start].title.substring(0, 20)}
+                {cardSong.title.substring(0, 20)}
               </p>
-              <p className="ps-3 text-white m-0">
-                {song[start].artist.name.substring(0, 20)}
+              <p className="ps-3 text-white-50 m-0">
+                {cardSong.artist.name.substring(0, 20)}
               </p>
             </div>
 
             <div className="play-icon0 shadow-sm my-auto ms-auto pe-3" id={idSong}>
               <div
                 className="circle"
-                onClick={() => songIdHandler(song[start].id, idSong)}
+                onClick={() => songIdHandler(cardSong.id, idSong)}
               >
-                {isPlaying && songId == song[start].id ? (
+                {isPlaying && songId == cardSong.id ? (
                   <div className="twoLine">
                     <div className="firstL"></div>
                     <div className="secondL"></div>
